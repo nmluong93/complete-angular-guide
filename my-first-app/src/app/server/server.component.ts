d05350bad795cd34d84cf2f7d6aa9a6dc995b0e9
luong <luongnm93@live.com>
2021-04-17 14:37:39 +0700

    ngStyle directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,7 +9,12 @@ export class ServerComponent implements OnInit {
 
   id = 10;
   serverStatus = 'offline';
+  
   ngOnInit(): void {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
+  getColor() {
+    return this.serverStatus === 'offline' ? 'red' : 'green';
   }
 }
