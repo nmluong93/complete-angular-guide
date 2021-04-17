72552d8aca8528cf0831d9f504dd639e88a08e98
luong <luongnm93@live.com>
2021-04-17 14:05:47 +0700

    align project with tutorial

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -7,27 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  allowAddServer = false;
-  serverCreatedStatus = '';
-  serverName = 'Test server';
-  serverCreated: boolean;
-  
-  constructor() { }
-
+  id = 10;
+  serverStatus = 'offline';
   ngOnInit(): void {
-    setTimeout(() => {
-      this.allowAddServer = true;
-    }, 2_000);
-  }
 
-  onAddServer() {
-    this.serverCreatedStatus = `Server was created! Name is ${this.serverName}` ;
-    this.serverCreated = true;
   }
-
-  onInputServerName(event : Event) {
-    console.log(event);
-    this.serverName = (<HTMLInputElement> event.target).value;
-  }
-
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  allowAddServer = false;
+  serverCreatedStatus = '';
+  serverName = 'Test server';
+  serverCreated: boolean;
+  
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2_000);
+  }
+
+  onAddServer() {
+    this.serverCreatedStatus = `Server was created! Name is ${this.serverName}` ;
+    this.serverCreated = true;
+  }
+
+  onInputServerName(event : Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement> event.target).value;
   }
 
 }
+
