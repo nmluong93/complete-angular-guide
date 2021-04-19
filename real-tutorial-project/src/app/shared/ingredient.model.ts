9e5ee1f5940ed7a7e556519e8158a45411e519e3
luongnm <luong.nguyen@adnovum.vn>
2021-04-19 10:45:14 +0700

    add Ingredient model

---
+++ real-tutorial-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+
+export class Ingredient {
+
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+    
+    // This is the same as code above
+    constructor(public name: string, public amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
