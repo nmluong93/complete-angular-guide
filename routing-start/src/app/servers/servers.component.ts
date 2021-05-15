e52bbabaedfdc834feb93d4b9d1cad7f45f1030a
luongnm <luongnm93@live.com>
2021-05-15 11:08:01 +0700

    Use router to navigate to url using routing in TS file with relative path

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute /*Currently navigated ROUTE*/) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // this will cause an error in console : main.js:1 ERROR Error: Uncaught (in promise): Error: Cannot match any routes.
+    // URL Segment: 'servers/servers'
+    // Because now we stay at route of ServersComponent ({relativeTo: this.route}) and try to add relative path 'servers'
+    // this.router.navigate(['servers'], {relativeTo: this.route});
+  }
 }
