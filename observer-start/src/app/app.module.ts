18f663951e48d319ea53d998e07b1f99a849a669
luong <luongnm93@live.com>
2021-05-17 21:01:57 +0700

    observable practice

--- observer-start/src/app/app.module.ts
+++ observer-start/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- observer-start/src/app/home/home.component.ts
+++ observer-start/src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
+
+  subscriber: Subscription;
 
   ngOnInit() {
+
+    const customedObs = Observable.create(observer => {
+      let i = 0;
+      setInterval(() => {
+        observer.next(i);
+        i++;
+        if (i === 2) {
+          observer.complete();
+        }
+        if (i > 3) {
+          observer.error(new Error('Error occurs'));
+
+        }
+      }, 1000);
+    });
+
+    this.subscriber = customedObs.subscribe(data => {
+      console.log(data);
+    },
+      error => {
+        console.log(`Error occurs in Observable ${error}`);
+        alert(error.message);
+      },
+      () => {
+        alert('Complete');
+      });
+
   }
 
 }
