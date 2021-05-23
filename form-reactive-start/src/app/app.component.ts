a297152977d8a2649998526c1214a9107dcf624a
luongnm <luongnm93@live.com>
2021-05-23 11:09:53 +0700

    Initialize a FormGroup

--- form-reactive-start/src/app/app.component.ts
+++ form-reactive-start/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
 
   signUpForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      // wrap the key in the quotation marks to make them can not be modified accidentally
+      'username': new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male')
+    });
+
+  }
+
 }
