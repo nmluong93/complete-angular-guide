e510a728d200c88d158e9c48d619a0564d84608a
luongnm <luongnm93@live.com>
2021-05-23 12:23:56 +0700

    FormGroup inside FormGroup

--- form-reactive-start/src/app/app.component.ts
+++ form-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +13,17 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      // wrap the key in the quotation marks to make them can not be modified accidentally
-      'username': new FormControl(null),
-      email: new FormControl(null),
-      gender: new FormControl('male')
+      userData: new FormGroup({
+        // wrap the key in the quotation marks to make them can not be modified accidentally
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      gender: new FormControl('male', Validators.required)
     });
 
   }
 
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
