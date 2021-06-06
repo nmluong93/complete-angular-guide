e23e0f2e7a1637421a0879d9d5f9022072093a60
luongnm <luong.nguyen@adnovum.vn>
2021-06-06 21:59:55 +0700

    Completing handling before and after HTTP request action in interceptor

--- http-project/src/app/auth-interceptor.service.ts
+++ http-project/src/app/auth-interceptor.service.ts
@@ -1,11 +1,20 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-export class AuthInterceptorService implements HttpInterceptor{
+export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // req.headers.append('', '');
+    const modifiedReq = req.clone({
+      headers: req.headers.append('Auth', 'key')
+    });
     console.log('AuthInterceptorService called');
-    return next.handle(req);
+    return next.handle(modifiedReq)
+      .pipe(tap(evt => {
+        console.log(evt);
+        if (evt.type === HttpEventType.Response) {
+          console.log(`Response arrives in HTTP interceptor ${evt.body}`);
+        }
+      }));
   }
 
 }
